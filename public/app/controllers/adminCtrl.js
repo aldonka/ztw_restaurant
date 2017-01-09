266c4dab330c2a00269696cc72250ca58f5ac7d5
Dominika <d.k.krzysztofik@gmail.com>
2017-01-09 09:40:19 +0100

    few fixes with login/getting users and reservation

--- public/app/controllers/adminCtrl.js
+++ public/app/controllers/adminCtrl.js
@@ -2,26 +2,27 @@
  * Created by Dominika on 2016-12-26.
  */
 angular.module('myApp')
-    .controller('adminCtrl', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
+    .controller('adminCtrl', ['$scope', '$rootScope', '$http', '$location', 'InfoService', 'AuthenticationService', function ($scope, $rootScope, $http, $location, InfoService, AuthenticationService) {
         $scope.users = (function () {
-            $http.get('http://localhost:3000/api/users').success(function(data){
+            $http.defaults.headers.common['Authorization'] = "Bearer " + AuthenticationService.getToken();
+            $http.get('http://localhost:3000/api/users').success(function (data) {
                 $scope.users = data;
             })
         })();
 
         $scope.registerNewUser = function () {
-            if($scope.user != null || $scope.user != undefined){
-                console.log('User register' + JSON.stringify($scope.user));
-                return $http.post('http://localhost:3000/api/register', $scope.user).success(function(data){
-                    console.log(data.token);
+            if ($scope.user != null || $scope.user != undefined) {
+                $http.defaults.headers.common['Authorization'] = "Bearer " + AuthenticationService.getToken();
+                return $http.post('http://localhost:3000/api/register', $scope.user).success(function (data) {
+                    InfoService.showSuccess("Utworzonego nowego użytkownika serwisu: " + $scope.user.username);
+                    $location.path('/admin/users');
                 });
             }
         };
 
         $scope.login = function () {
-            if($scope.user != null || $scope.user != undefined){
-                console.log('User register' + JSON.stringify($scope.user));
-                return $http.post('http://localhost:3000/api/login', $scope.user).success(function(data){
+            if ($scope.user != null || $scope.user != undefined) {
+                return $http.post('http://localhost:3000/api/login', $scope.user).success(function (data) {
                     console.log("Success" + data);
                 });
             }
--- public/app/controllers/menuCtrl.js
+++ public/app/controllers/menuCtrl.js
@@ -2,14 +2,16 @@
  * Created by Dominika on 2016-12-26.
  */
 angular.module('myApp')
-    .controller('menuCtrl', ['$scope', '$rootScope', '$location', 'DishService', 'CategoriesService', 'InfoService', 'Socket', '$location', function ($scope, $rootScope, $location, DishService, CategoriesService, InfoService, Socket, $location) {
+    .controller('menuCtrl', ['$scope', '$rootScope', '$location', 'DishService', 'CategoriesService', 'InfoService', 'Socket', '$window', function ($scope, $rootScope, $location, DishService, CategoriesService, InfoService, Socket, $window) {
         Socket.on('dish:added', function(data){
             InfoService.showInfo("Dodano nową potrawe: " + data.name);
             getAllDishes();
+
         });
         Socket.on('dish:modified', function(data){
             InfoService.showInfo("Właśnie zmodyfikowano potrawę: " + data.name);
             getAllDishes();
+            $window.location.reload();
         });
 
         function getAllDishes() {
